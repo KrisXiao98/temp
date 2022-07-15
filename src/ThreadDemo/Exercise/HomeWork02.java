5c2ab5c7238676ade422dd787fdda49917b69cce
KrisXiao <xiaokai_v5@163.com>
2022-07-15 22:30:40 +0800

    多线程作业更新

--- src/ThreadDemo/Exercise/HomeWork02.java
+++ src/ThreadDemo/Exercise/HomeWork02.java
@@ -8,8 +8,37 @@ package ThreadDemo.Exercise;
  */
 public class HomeWork02 {
     public static void main(String[] args) {
-
+        Money money1 = new Money();
+        Thread thread1 = new Thread(money1);
+        Thread thread2 = new Thread(money1);
+        thread1.setName("t1");
+        thread2.setName("t2");
+        thread1.start();
+        thread2.start();
     }
 }
 
+class Money implements Runnable {
 
+    private int balanceMoney = 10000;
+
+    public void withDrawMoney(){
+        synchronized (this){
+            while (true){
+                if (balanceMoney >= 1000){
+                    balanceMoney -= 1000;
+                    System.out.println(Thread.currentThread().getName() + "取走1000块，还剩下" + balanceMoney);
+                }else {
+                    System.out.println("不够取钱1000的了。");
+                    break;
+                }
+            }
+        }
+    }
+
+    @Override
+    public void run() {
+        withDrawMoney();
+    }
+
+}
